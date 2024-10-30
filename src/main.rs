cb3d62d348680b733513797b3e9fa4f8437b57de
fredy <11772686@qq.com>
2024-10-30 20:01:00 +0800

    tmp

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use std::env;
-use tokio::net::TcpStream;
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[0];
-
+    
+    
 
     println!("Hello, world!");
 }
-
-
